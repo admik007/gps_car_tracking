f8d5f01e48a3d537597cbcf0ade51ada8b985007
Tomas KOVAC <office@ztk-comp.sk>
2021-04-25 14:02:26 +0200

    Update locations.php

--- server/locations.php
+++ server/locations.php
@@ -6,11 +6,9 @@
   if (($entries['lat'] > '48.722') and ($entries['lat'] < '48.724') and ($entries['lon'] > '21.227') and ($entries['lon'] < '21.229')) { $bgmiesto='#ffffb0';$miesto='KE-Benzinka-Popradska';}
   if (($entries['lat'] > '48.739') and ($entries['lat'] < '48.740') and ($entries['lon'] > '21.255') and ($entries['lon'] < '21.256')) { $bgmiesto='#ffffa0';$miesto='KE-Benzinka-Hlinkova';}
   if (($entries['lat'] > '48.740') and ($entries['lat'] < '48.741') and ($entries['lon'] > '21.256') and ($entries['lon'] < '21.257')) { $bgmiesto='#ffff90';$miesto='KE-Studentska-Parkovisko';}
-  if (($entries['lat'] > '48.576') and ($entries['lat'] < '48.578') and ($entries['lon'] > '21.331') and ($entries['lon'] < '21.332')) { $bgmiesto='#ffff50';$miesto='Trstene - Jarka';}
   if (($entries['lat'] > '48.715') and ($entries['lat'] < '48.716') and ($entries['lon'] > '21.213') and ($entries['lon'] < '21.214')) { $bgmiesto='#ffff40';$miesto='KE-KVP-Billa';}
   if (($entries['lat'] > '48.715') and ($entries['lat'] < '48.716') and ($entries['lon'] > '21.217') and ($entries['lon'] < '21.218')) { $bgmiesto='#ffff30';$miesto='KE-KVP-Posta';}
   if (($entries['lat'] > '48.719') and ($entries['lat'] < '48.720') and ($entries['lon'] > '21.212') and ($entries['lon'] < '21.214')) { $bgmiesto='#ffff20';$miesto='KE-KVP-Lidl';}
-  if (($entries['lat'] > '48.705') and ($entries['lat'] < '48.706') and ($entries['lon'] > '21.236') and ($entries['lon'] < '21.237')) { $bgmiesto='#ffff00';$miesto='KE-Kucharik';}
   if (($entries['lat'] > '48.721') and ($entries['lat'] < '48.722') and ($entries['lon'] > '21.266') and ($entries['lon'] < '21.268')) { $bgmiesto='#fffef0';$miesto='KE-Stanica-Taxi';}
   if (($entries['lat'] > '48.722') and ($entries['lat'] < '48.724') and ($entries['lon'] > '21.266') and ($entries['lon'] < '21.268')) { $bgmiesto='#fffee0';$miesto='KE-Stanica-Zadna-Cast';}
   if (($entries['lat'] > '48.714') and ($entries['lat'] < '48.716') and ($entries['lon'] > '21.237') and ($entries['lon'] < '21.239')) { $bgmiesto='#fffed0';$miesto='KE-Galeria';}
@@ -18,7 +16,6 @@
   if (($entries['lat'] > '48.693') and ($entries['lat'] < '48.696') and ($entries['lon'] > '21.236') and ($entries['lon'] < '21.240')) { $bgmiesto='#fffeb0';$miesto='KE-Optima';}
   if (($entries['lat'] > '48.712') and ($entries['lat'] < '48.713') and ($entries['lon'] > '21.209') and ($entries['lon'] < '21.210')) { $bgmiesto='#fffea0';$miesto='KE-KVP-Autoumyvarka';}
   if (($entries['lat'] > '48.689') and ($entries['lat'] < '48.690') and ($entries['lon'] > '21.262') and ($entries['lon'] < '21.263')) { $bgmiesto='#fffe90';$miesto='KE-TempusBMW';}
-  if (($entries['lat'] > '48.553') and ($entries['lat'] < '48.554') and ($entries['lon'] > '21.971') and ($entries['lon'] < '21.972')) { $bgmiesto='#fffe60';$miesto='Janik-Otilia';}
   if (($entries['lat'] > '48.735') and ($entries['lat'] < '48.736') and ($entries['lon'] > '21.206') and ($entries['lon'] < '21.207')) { $bgmiesto='#fffe50';$miesto='KE-Watsonova-Policia';}
   if (($entries['lat'] > '48.721') and ($entries['lat'] < '48.722') and ($entries['lon'] > '21.216') and ($entries['lon'] < '21.217')) { $bgmiesto='#fffe40';$miesto='KE-Ku-Cickam';}
   if (($entries['lat'] > '48.668') and ($entries['lat'] < '48.669') and ($entries['lon'] > '21.234') and ($entries['lon'] < '21.235')) { $bgmiesto='#fffe10';$miesto='KE-Letisko-Korculovanie';}
@@ -35,7 +32,6 @@
   if (($entries['lat'] > '49.061') and ($entries['lat'] < '49.063') and ($entries['lon'] > '20.218') and ($entries['lon'] < '20.219')) { $bgmiesto='#fffd60';$miesto='Svit - Benzinka Gulf -> Poprad';}
   if (($entries['lat'] > '49.057') and ($entries['lat'] < '49.060') and ($entries['lon'] > '20.264') and ($entries['lon'] < '20.268')) { $bgmiesto='#fffd50';$miesto='Poprad - Tesco';}
   if (($entries['lat'] > '48.740') and ($entries['lat'] < '48.745') and ($entries['lon'] > '21.265') and ($entries['lon'] < '21.268')) { $bgmiesto='#fffd40';$miesto='KE-Tesco';}
-  if (($entries['lat'] > '48.854') and ($entries['lat'] < '48.857') and ($entries['lon'] > '21.090') and ($entries['lon'] < '21.093')) { $bgmiesto='#fffd30';$miesto='KE-Ruzin Tutkovci';}
   if (($entries['lat'] > '48.728') and ($entries['lat'] < '48.730') and ($entries['lon'] > '21.255') and ($entries['lon'] < '21.257')) { $bgmiesto='#fffd20';$miesto='KE-Hotel Yasmin';}
   if (($entries['lat'] > '48.718') and ($entries['lat'] < '48.720') and ($entries['lon'] > '21.264') and ($entries['lon'] < '21.266')) { $bgmiesto='#fffd00';$miesto='KE-Aupark';}
   if (($entries['lat'] > '48.712') and ($entries['lat'] < '48.713') and ($entries['lon'] > '21.211') and ($entries['lon'] < '21.212')) { $bgmiesto='#fffcf0';$miesto='KE-Fresh Klimkovicova';}
@@ -47,7 +43,6 @@
   if (($entries['lat'] > '49.125') and ($entries['lat'] < '49.128') and ($entries['lon'] > '20.072') and ($entries['lon'] < '20.074')) { $bgmiesto='#fffc50';$miesto='Popradske Pleso';}
   if (($entries['lat'] > '49.048') and ($entries['lat'] < '49.050') and ($entries['lon'] > '20.290') and ($entries['lon'] < '20.292')) { $bgmiesto='#fffc40';$miesto='PP - MAX';}
   if (($entries['lat'] > '49.083') and ($entries['lat'] < '49.091') and ($entries['lon'] > '20.070') and ($entries['lon'] < '20.072')) { $bgmiesto='#fffc30';$miesto='Tatranska Strba';}
-  if (($entries['lat'] > '49.090') and ($entries['lat'] < '49.091') and ($entries['lon'] > '20.070') and ($entries['lon'] < '20.072')) { $bgmiesto='#fffc20';$miesto='Tatranska Strba - Krstna';}
   if (($entries['lat'] > '49.056') and ($entries['lat'] < '49.058') and ($entries['lon'] > '20.302') and ($entries['lon'] < '20.304')) { $bgmiesto='#fffc10';$miesto='PP - Surprise';}
   if (($entries['lat'] > '49.067') and ($entries['lat'] < '49.068') and ($entries['lon'] > '20.314') and ($entries['lon'] < '20.316')) { $bgmiesto='#fffc00';$miesto='Spisska Sobota - Borievka';}
   if (($entries['lat'] > '48.740') and ($entries['lat'] < '48.742') and ($entries['lon'] > '19.185') and ($entries['lon'] < '19.186')) { $bgmiesto='#fffbf0';$miesto='BB - Benzinka Shell';}
@@ -63,16 +58,9 @@
   if (($entries['lat'] > '48.669') and ($entries['lat'] < '48.670') and ($entries['lon'] > '21.271') and ($entries['lon'] < '21.273')) { $bgmiesto='#fffb50';$miesto='KE-Benzinka OMV - VSS';}
   if (($entries['lat'] > '48.688') and ($entries['lat'] < '48.689') and ($entries['lon'] > '21.266') and ($entries['lon'] < '21.267')) { $bgmiesto='#fffb40';$miesto='KE-Krizovatka VSS';}
   if (($entries['lat'] > '48.726') and ($entries['lat'] < '48.727') and ($entries['lon'] > '21.244') and ($entries['lon'] < '21.245')) { $bgmiesto='#fffb30';$miesto='KE-Parkovisko Lekarka';}
-  if (($entries['lat'] > '48.648') and ($entries['lat'] < '48.649') and ($entries['lon'] > '21.569') and ($entries['lon'] < '21.570')) { $bgmiesto='#fffb20';$miesto='Zemplinska Teplica - Peto';}
-  if (($entries['lat'] > '48.645') and ($entries['lat'] < '48.646') and ($entries['lon'] > '21.569') and ($entries['lon'] < '21.571')) { $bgmiesto='#fffb10';$miesto='Zemplinska Teplica - Rodicia';}
-  if (($entries['lat'] > '48.750') and ($entries['lat'] < '48.751') and ($entries['lon'] > '21.348') and ($entries['lon'] < '21.349')) { $bgmiesto='#fffb00';$miesto='Rozhanovce - Kusnirovci';}
-  if (($entries['lat'] > '48.685') and ($entries['lat'] < '48.686') and ($entries['lon'] > '21.288') and ($entries['lon'] < '21.289')) { $bgmiesto='#fffaf0';$miesto='KE-Tonka';}
   if (($entries['lat'] > '48.714') and ($entries['lat'] < '48.716') and ($entries['lon'] > '21.215') and ($entries['lon'] < '21.216')) { $bgmiesto='#fffae0';$miesto='KE-Lekar KVP';}
   if (($entries['lat'] > '48.700') and ($entries['lat'] < '48.701') and ($entries['lon'] > '21.223') and ($entries['lon'] < '21.224')) { $bgmiesto='#fffab0';$miesto='KE-AquaReina';}
-  if (($entries['lat'] > '49.136') and ($entries['lat'] < '49.137') and ($entries['lon'] > '20.418') and ($entries['lon'] < '20.420')) { $bgmiesto='#fffa90';$miesto='KK-Slavkovska_24';}
-  if (($entries['lat'] > '48.718') and ($entries['lat'] < '48.719') and ($entries['lon'] > '21.235') and ($entries['lon'] < '21.236')) { $bgmiesto='#fffa80';$miesto='KE-Lesnicka 29 - Miska';}
   if (($entries['lat'] > '48.698') and ($entries['lat'] < '48.699') and ($entries['lon'] > '21.236') and ($entries['lon'] < '21.237')) { $bgmiesto='#fffa50';$miesto='KE-Benzinka Slovnaft - Moldavska';}
-  if (($entries['lat'] > '48.733') and ($entries['lat'] < '48.734') and ($entries['lon'] > '21.286') and ($entries['lon'] < '21.287')) { $bgmiesto='#fffa30';$miesto='KE-Stefan_Danko';}
   if (($entries['lat'] > '48.730') and ($entries['lat'] < '48.732') and ($entries['lon'] > '21.290') and ($entries['lon'] < '21.291')) { $bgmiesto='#fffa00';$miesto='KE-Benzinka Jet - Furca';}
   if (($entries['lat'] > '48.736') and ($entries['lat'] < '48.737') and ($entries['lon'] > '21.245') and ($entries['lon'] < '21.247')) { $bgmiesto='#fff9f0';$miesto='KE-Hlink/Kom/Wat';}
   if (($entries['lat'] > '48.713') and ($entries['lat'] < '48.714') and ($entries['lon'] > '21.235') and ($entries['lon'] < '21.236')) { $bgmiesto='#fff9e0';$miesto='KE-Toryska/SNP';}
@@ -100,12 +88,10 @@
   if (($entries['lat'] > '49.309') and ($entries['lat'] < '49.310') and ($entries['lon'] > '20.679') and ($entries['lon'] < '20.680')) { $bgmiesto='#fff870';$miesto='SL-Slovnaft';}
   if (($entries['lat'] > '48.713') and ($entries['lat'] < '48.714') and ($entries['lon'] > '21.230') and ($entries['lon'] < '21.231')) { $bgmiesto='#fff860';$miesto='KE-Poliklinika_Terasa';}
   if (($entries['lat'] > '48.720') and ($entries['lat'] < '48.721') and ($entries['lon'] > '21.212') and ($entries['lon'] < '21.213')) { $bgmiesto='#fff850';$miesto='KE-Za_Lidlom_KVP';}
-  if (($entries['lat'] > '49.141') and ($entries['lat'] < '49.142') and ($entries['lon'] > '20.430') and ($entries['lon'] < '20.432')) { $bgmiesto='#fff840';$miesto='KK-Nizna_Brana_5';}
   if (($entries['lat'] > '48.703') and ($entries['lat'] < '48.704') and ($entries['lon'] > '21.245') and ($entries['lon'] < '21.246')) { $bgmiesto='#fff830';$miesto='KE-Slovnaft_Alejova';}
   if (($entries['lat'] > '48.922') and ($entries['lat'] < '48.923') and ($entries['lon'] > '21.462') and ($entries['lon'] < '21.463')) { $bgmiesto='#fff820';$miesto='Dubnik-Vysielac';}
   if (($entries['lat'] > '48.801') and ($entries['lat'] < '48.802') and ($entries['lon'] > '21.397') and ($entries['lon'] < '21.398')) { $bgmiesto='#fff810';$miesto='Cizatice-TowerCom';}
   if (($entries['lat'] > '48.732') and ($entries['lat'] < '48.740') and ($entries['lon'] > '21.241') and ($entries['lon'] < '21.243')) { $bgmiesto='#fff800';$miesto='Parkovisko Bozeny Nemcovej';}
-  if (($entries['lat'] > '48.713') and ($entries['lat'] < '48.715') and ($entries['lon'] > '21.211') and ($entries['lon'] < '21.213')) { $bgmiesto='#fff7F0';$miesto='KE-Bauerova 22';}
   if (($entries['lat'] > '48.706') and ($entries['lat'] < '48.708') and ($entries['lon'] > '21.253') and ($entries['lon'] < '21.255')) { $bgmiesto='#fff7E0';$miesto='KE-Stara Nemocnica';}
   if (($entries['lat'] > '48.594') and ($entries['lat'] < '48.596') and ($entries['lon'] > '21.028') and ($entries['lon'] < '21.032')) { $bgmiesto='#fff7D0';$miesto='Mokrance';}
   if (($entries['lat'] > '48.629') and ($entries['lat'] < '48.630') and ($entries['lon'] > '21.171') and ($entries['lon'] < '21.172')) { $bgmiesto='#fff7C0';$miesto='Saca-Slovnaft';}
