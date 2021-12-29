2dc1ff6517605bcce3069e8539d694d966301e97
Tomas KOVAC <office@ztk-comp.sk>
2021-12-29 11:15:53 +0100

    Update get_only_gps.py

--- node/get_only_gps.py
+++ node/get_only_gps.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python -tt
 
 import datetime as dt
-import time
+from time import sleep
 import gps
 import subprocess
 import os
 
-session = gps.gps("localhost", "9999")
+session = gps.gps("127.0.0.1", "9999")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+
+f= open("/tmp/gps.gpx","w+") #yyyymmdd
+f.write("lat=0.0\nlon=0.0\nalt=0.0\nsats=0\n")
+f.close()
 
 out = subprocess.Popen("cat /proc/cpuinfo | grep Serial | cut -d ' ' -f2 | tr -d '\n' | tr -d '\r'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 device,stderr = out.communicate()
@@ -18,10 +22,11 @@ cputemp,stderr = out.communicate()
 out = subprocess.Popen("cat /proc/loadavg | awk {'print $1'} | tr -d '\n' | tr -d '\r'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 load,stderr = out.communicate()
 
+print('Script started');
+
 while True:
  try:
   report = session.next()
-  print report
   if report['class'] == 'SKY':          # Reportuje pocet satelitov
    if hasattr(report, 'satellites'):
     num_sats = 0
@@ -29,6 +34,7 @@ while True:
      if hasattr(satellite, 'used') and satellite.used:
       num_sats += 1
 
+#  num_sats = 0
   if report['class'] == 'TPV':          # Reportuje lat, lon, alt, track, speed
    if hasattr(report, 'time'):
     if hasattr(report, 'lat'):
@@ -65,17 +71,19 @@ while True:
      mode = report.mode
     else:
      mode = 0
-    print("Lat: "+str(round(lat,6))+"; Lon: "+str(round(lon,6))+"; Alt: "+str(round(alt,0))+"; Time: "+report.time+"; Speed: "+str(round(speed))+"; Track: "+str(round(track))+"; Climb: "+str(round(climb,0))+"; Mode: "+str(round(mode,0))+"; Sat: "+str(num_sats)+"; Device: "+device+"; Temp: "+cputemp+"; Load: "+(load)+"")
+
+    print("Lat: "+str(round(lat,6))+"; Lon: "+str(round(lon,6))+"; Alt: "+str(round(alt,0))+"; Time: "+report.time+"; Speed: "+str(round(speed))+"; Track: "+str(round(track))+"; Climb: "+str(round(climb,0))+"; Mode: "+str(round(mode,0))+"; Sat: "+str(num_sats)+"; Device: "+device+"; Temp: "+cputemp+"; Load: "+(load)+"\n")
 
 #    f= open("/mnt/{}.gpx".format(dt.datetime.now().strftime("%Y%m%d")),"a+") #yyyymmdd
 #    f.write("<trkpt lat=\""+str(round(lat,8))+"\" lon=\""+str(round(lon,8))+"\"><ele>"+str(round(alt,0))+"</ele><time>"+report.time+"</time><speed>"+str(round(speed))+"</speed><src>gps</src><sat>"+str(num_sats)+"</sat> <devicerpi>"+device+"</devicerpi><temp>"+cputemp+"</temp><load>"+load+"</load></trkpt>\n")
 #    f.close()
-    f= open("/tmp/gps.gpx","w")
-    f.write("LAT=+"+str(round(lat,8))+"\nLON=+"+str(round(lon,8)))
+
+    f= open("/tmp/gps.gpx","w+") #yyyymmdd
+    f.write("lat="+str(round(lat,8))+"\nlon="+str(round(lon,8))+"\nalt="+str(round(alt,0))+"\ntime="+report.time+"\nspeed="+str(round(speed))+"\nsats="+str(num_sats))
     f.close()
 
-    os.system("/usr/bin/curl -s \"http://DEFINE_YOUR_SERVER/?lat="+str(round(lat,8))+"&lon="+str(round(lon,8))+"&time="+report.time+"&spd="+str(round(speed))+"&sat="+str(num_sats)+"&alt="+str(round(alt,0))+"&bat=100.0&acc=10.0&provider=gps&direction="+str(round(track))+"&device=NODE08&devicerpi="+device+"&temprpi="+cputemp+"&loadrpi="+load+"\" -o /dev/null")
-    break
+    os.system("/usr/bin/curl -s \"http://URL.DOMAIN.COM/?lat="+str(round(lat,8))+"&lon="+str(round(lon,8))+"&time="+report.time+"&spd="+str(round(speed))+"&sat="+str(num_sats)+"&alt="+str(round(alt,0))+"&bat=100.0&acc=10.0&provider=gps&direction="+str(round(track))+"&devicerpi="+device+"&temprpi="+cputemp+"&loadrpi="+load+"\" -o /dev/null")
+#    break
 
  except KeyError:
   pass
