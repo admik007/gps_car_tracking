7947b887e563f240803068753d680204149dcb2b
Tomas KOVAC <office@ztk-comp.sk>
2021-12-29 11:23:30 +0100

    Update mysql.sql

--- database/mysql.sql
+++ database/mysql.sql
@@ -3,11 +3,22 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hostiteľ: localhost:3306
--- Verzia serveru: 10.3.25-MariaDB-0+deb10u1
+-- Čas generovania: St 29.Dec 2021, 11:22
+-- Verzia serveru: 10.3.31-MariaDB-0+deb10u1
 -- Verzia PHP: 5.6.40-0+deb8u12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databáza: `ztk_comp_sk`
+--
 
 -- --------------------------------------------------------
 
@@ -25,33 +36,44 @@ CREATE TABLE `gps_miesto` (
 -- --------------------------------------------------------
 
 --
+-- Štruktúra tabuľky pre tabuľku `gps_provider`
+--
+
+CREATE TABLE `gps_provider` (
+  `ip` varchar(15) NOT NULL,
+  `provider` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Štruktúra tabuľky pre tabuľku `gps_tracking`
 --
 
 CREATE TABLE `gps_tracking` (
   `id` int(8) NOT NULL,
-  `lat` varchar(14) NOT NULL DEFAULT '',
-  `lon` varchar(14) NOT NULL DEFAULT '',
-  `alt` varchar(10) NOT NULL DEFAULT '',
-  `acc` varchar(10) NOT NULL DEFAULT '',
-  `spd` varchar(10) NOT NULL DEFAULT '',
-  `sat` varchar(2) NOT NULL DEFAULT '',
-  `time` varchar(26) NOT NULL DEFAULT '',
-  `bat` varchar(5) NOT NULL DEFAULT '',
-  `ip` varchar(15) NOT NULL DEFAULT '',
-  `year` varchar(4) NOT NULL DEFAULT '',
-  `month` char(2) NOT NULL DEFAULT '',
-  `day` char(2) NOT NULL DEFAULT '',
-  `hour` char(2) NOT NULL DEFAULT '',
-  `minute` char(2) NOT NULL DEFAULT '',
-  `second` char(2) NOT NULL DEFAULT '',
-  `device` varchar(255) NOT NULL DEFAULT '',
-  `provider` char(10) NOT NULL,
-  `direction` char(10) NOT NULL,
-  `devicerpi` char(16) NOT NULL,
-  `temprpi` char(10) NOT NULL,
-  `loadrpi` char(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `lat` varchar(14) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `lon` varchar(14) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `alt` varchar(10) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `acc` varchar(10) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `spd` varchar(10) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `sat` varchar(2) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `time` varchar(26) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `bat` varchar(5) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `ip` varchar(15) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `year` varchar(4) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `month` char(2) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `day` char(2) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `hour` char(2) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `minute` char(2) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `second` char(2) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `device` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `provider` char(10) CHARACTER SET latin1 NOT NULL,
+  `direction` char(10) CHARACTER SET latin1 NOT NULL,
+  `devicerpi` char(16) CHARACTER SET latin1 NOT NULL,
+  `temprpi` char(10) CHARACTER SET latin1 NOT NULL,
+  `loadrpi` char(11) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -92,7 +114,8 @@ CREATE TABLE `gps_tracking_archive` (
 -- Indexy pre tabuľku `gps_tracking`
 --
 ALTER TABLE `gps_tracking`
-  ADD UNIQUE KEY `id` (`id`,`time`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT pre exportované tabuľky
@@ -102,4 +125,7 @@ ALTER TABLE `gps_tracking`
 -- AUTO_INCREMENT pre tabuľku `gps_tracking`
 --
 ALTER TABLE `gps_tracking`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22550627;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
