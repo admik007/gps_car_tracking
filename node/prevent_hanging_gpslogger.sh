29594cceabdf2936c7f1ce197206448a84cd5f27
Tomas KOVAC <office@ztk-comp.sk>
2021-12-29 11:13:21 +0100

    Update prevent_hanging_gpslogger.sh

--- node/prevent_hanging_gpslogger.sh
+++ node/prevent_hanging_gpslogger.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+# Check if GPSD is running
+GPSD=`ps -ef | grep 9999| grep -v grep | wc -l`
+if [ "${GPSD}" -eq "0" ]; then
+ /root/scripts/gps_activate.sh &
+fi
+
 # Check for running process. If more, kill it. Prevent hanging
-ITSELF=`ps -ef | grep prevent_hanging_gpslogger | grep -v grep | wc -l`
+ITSELF=`ps -ef | grep prevent_hanging_gpslogger | egrep -v "grep|vi" | wc -l`
 if [ "${ITSELF}" -gt "4" ]; then
- for i in `ps -ef | grep "prevent_hanging_gpslogger" | grep -v grep | grep -v vi | awk {'print $2'}`; do
+ for i in `ps -ef | grep "prevent_hanging_gpslogger" | egrep -v "grep|vi" | awk {'print $2'}`; do
   kill -9 $i
  done
 fi
 
-PROCESSES_NR=`ps -ef | grep "python /root/scripts/get_only_gps.py" | grep -v grep | wc -l`
-if [ "${PROCESSES_NR}" -gt "1" ]; then
+PROCESSES_NR=`ps -ef | grep "python /root/scripts/get_only_gps.py" | grep -v SCREEN | grep -v grep | wc -l`
+if [ "${PROCESSES_NR}" -ge "1" ]; then
  for i in `ps -ef | grep "python /root/scripts/get_only_gps.py" | grep -v grep | awk {'print $2'}`; do
   kill -9 $i
   logger -p info "Killed process (get_only_gps) - ${i}"
  done
+ screen -S GET_GPS -dm python /root/scripts/get_only_gps.py
 else
- python /root/scripts/get_only_gps.py
+ screen -S GET_GPS -dm python /root/scripts/get_only_gps.py
 fi
