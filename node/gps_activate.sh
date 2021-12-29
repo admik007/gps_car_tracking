92fa42445fbc99d6ddb6f460eb7f9a0ba7ad0a75
Tomas KOVAC <office@ztk-comp.sk>
2021-12-29 11:13:44 +0100

    Update gps_activate.sh

--- node/gps_activate.sh
+++ node/gps_activate.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #stty -F /dev/ttyS0 -echo
-gpsd /dev/ttyS0 -nb -S 9999 -G -F /var/run/gpsd.sock
+MODEL=`cat /proc/cpuinfo | grep Model | cut -d ':' -f2 | cut -d ' ' -f1,2,3,4`
+if [ "${MODEL}" == " Raspberry Pi Zero" ]; then
+ gpsd /dev/ttyS0 -nb -S 9999 -G -F /var/run/gpsd.sock
+else
+ gpsd /dev/ttyAMA0 -nb -S 9999 -G -F /var/run/gpsd.sock
+fi
